66045b2ef9b4411864ea57e22e74aca6cf16698d
Eric O'Callaghan <eric@ericoc.com>
2023-11-30 18:44:38 -0500

    fix allowed_hosts

--- settings.example.py
+++ settings.example.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'SECRET-KEY'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['djadmin@gaw.sh']
+ALLOWED_HOSTS = ['djadmin.gaw.sh']
 
 ADMINS = MANAGERS = (("Eric OC", "eric@ericoc.com"),)
 
