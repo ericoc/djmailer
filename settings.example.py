177cd718b93942b3fac9a85c4e70be5297ccb00e
Eric O'Callaghan <eric@ericoc.com>
2023-12-17 13:55:08 -0500

    x-frame-options sameorigin

--- settings.example.py
+++ settings.example.py
@@ -84,6 +84,8 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'wsgi.application'
 
+X_FRAME_OPTIONS = "SAMEORIGIN"
+
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 DATABASES = {
